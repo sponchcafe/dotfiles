4abc7644ccf9e4fb6e71ad7d13812343722f6ce3
Till Zickmantel <zickmantel@physik.uni-luebeck.de>
2018-03-06 09:52:58 +0100

    Added pathogen install script

---
+++ get_pathogen.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir ~/.vim/autoload
+mkdir ~/.vim/pathogen
+
+curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
+
