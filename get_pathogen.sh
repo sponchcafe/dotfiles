eaa09f1b7ee0a827ae7e910f85732f5b552f9c17
Till <till.zickmantel@gmail.com>
2018-10-01 11:00:53 +0200

    added bundle dir

--- get_pathogen.sh
+++ get_pathogen.sh
@@ -2,6 +2,7 @@
 
 mkdir ~/.vim/autoload
 mkdir ~/.vim/pathogen
+mkdir ~/.vim/bundle
 
 curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim > ~/.vim/autoload/pathogen.vim
 
