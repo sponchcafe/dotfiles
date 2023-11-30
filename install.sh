2d811068198bb63457508a594356dfcb0e52cb99
Till Zickmantel <till.zickmantel@gmail.com>
2023-11-30 20:26:31 +0100

    CHG: Update install script for neovim

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-if [  ! -d ~/.vim/bundle ]; then
-    echo "Creating ~/.vim/bundle"
-    mkdir -p ~/.vim/bundle
-fi
-
 if [  ! -d ~/.config/nvim ]; then
     echo "Creating ~/.config/nvim"
     mkdir -p ~/.config/nvim
@@ -14,17 +9,11 @@ echo "Installing .config.fish"
 cp shell/fish/config.fish ~/.config/fish/config.fish
 mkdir -p ~/.config/fish/functions && cp -R shell/fish/functions/* ~/.config/fish/functions/
 
-echo "Installing .vimrc"
-cp vim/vimrc ~/.vimrc
+echo "Installing init.lua"
+cp nvim/init.lua ~/.config/nvim
 
 echo "Installing .tmux.conf"
 cp tmux/tmux.conf ~/.tmux.conf
-
-echo "Installing nvim config"
-cp vim/init.vim ~/.config/nvim/init.vim
-
-echo "Installing bundles"
-nvim +PlugInstall +qall
 
 # powerline
 pip install --user -r shell/powerline/requirements.txt
